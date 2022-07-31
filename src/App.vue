d226d7fb9a9de64ee060101d05de745b082646f6
Daniel Mendes Sechinske <mendes.sechinske@gmail.com>
2022-07-31 17:17:34 -0300

    Added initial HTML componentes to format on project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+  // This starter template is using Vue 3 <script setup> SFCs
+  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+  import BaseForm from './components/BaseForm.vue';
 </script>
 
 <template>
-  <div>Hello!</div>
+  <BaseForm class="base-form" />
 </template>
 
 <style scoped></style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,11 @@
+<template>
+  <input />
+</template>
+
+<script>
+  export default {
+    name: 'BaseInput',
+  };
+</script>
+
+<style scoped></style>
