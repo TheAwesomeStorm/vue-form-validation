37fbc19bd8a6a75e05a6f228811870945f7fa6b5
Daniel Mendes Sechinske <mendes.sechinske@gmail.com>
2022-08-03 17:01:19 -0300

    Encapsulated FormSample component in a HTML form element

--- src/components/FormSample.vue
+++ src/components/FormSample.vue
@@ -1,97 +1,95 @@
 <template>
-  <BaseTextTypeInput
-    type="text"
-    :label="$t('form.name')"
-    v-model="userData.name"
-    :errors="v$.userData.name.$errors"
-    icons-placement="has-icons-left has-icons-right"
-    :is-dirty="v$.userData.name.$dirty"
-    :is-valid="!v$.userData.name.$invalid"
-  >
-    <span class="icon is-small is-left">
-      <i class="fas fa-user"></i>
-    </span>
-    <span
-      v-if="!v$.userData.name.$invalid && v$.userData.name.$dirty"
-      class="icon is-small is-right"
+  <form @submit.prevent="validate">
+    <BaseTextTypeInput
+      type="text"
+      :label="$t('form.name')"
+      v-model="userData.name"
+      :errors="v$.userData.name.$errors"
+      icons-placement="has-icons-left has-icons-right"
+      :is-dirty="v$.userData.name.$dirty"
+      :is-valid="!v$.userData.name.$invalid"
     >
-      <i class="fas fa-check"></i>
-    </span>
-    <span
-      v-if="v$.userData.name.$invalid && v$.userData.name.$dirty"
-      class="icon is-small is-right"
-    >
-      <i class="fas fa-times"></i>
-    </span>
-  </BaseTextTypeInput>
-  <BaseTextTypeInput
-    type="number"
-    :label="$t('form.age')"
-    v-model="userData.age"
-    :errors="v$.userData.age.$errors"
-  />
-  <BaseTextTypeInput
-    :label="$t('form.email')"
-    type="email"
-    v-model="userData.email"
-    :errors="v$.userData.email.$errors"
-    icons-placement="has-icons-left"
-  >
-    <span class="icon is-small is-left">
-      <i class="fas fa-envelope"></i>
-    </span>
-  </BaseTextTypeInput>
-  <BaseSelect
-    :options="[
-      $t('form.gender.options.male'),
-      $t('form.gender.options.female'),
-      $t('form.gender.options.non-binary'),
-    ]"
-    :label="$t('form.gender.description')"
-    v-model="userData.gender"
-    :placeholder="$t('form.gender.options.placeholder')"
-    :errors="v$.userData.gender.$errors"
-  />
-  <BaseTextArea
-    :label="$t('form.message.label')"
-    :placeholder="$t('form.message.placeholder')"
-    v-model="userData.comments"
-    :errors="v$.userData.comments.$errors"
-  />
-  <BaseMultipleCheckbox
-    :question="$t('form.offer.description')"
-    :options="['Google', 'LinkedIn', 'Facebook', 'Instagram', 'Twitter']"
-    v-model="userData.marketing"
-  />
-  <BaseCheckbox
-    :label="$t('form.newsletter')"
-    v-model="userData.subscribe"
-    :errors="v$.userData.subscribe.$errors"
-  />
-  <BaseRadio
-    name="offer"
-    :options="[$t('form.terms.options.yes'), $t('form.terms.options.no')]"
-    :question="$t('form.terms.description')"
-    v-model="userData.terms"
-    :errors="v$.userData.terms.$errors"
-  />
-
-  <div class="field is-grouped">
-    <div class="control">
-      <button
-        class="button is-link"
-        @click="validate"
-        :disabled="isFormInvalid"
+      <span class="icon is-small is-left">
+        <i class="fas fa-user"></i>
+      </span>
+      <span
+        v-if="!v$.userData.name.$invalid && v$.userData.name.$dirty"
+        class="icon is-small is-right"
       >
-        {{ $t('form.buttons.submit') }}
-      </button>
+        <i class="fas fa-check"></i>
+      </span>
+      <span
+        v-if="v$.userData.name.$invalid && v$.userData.name.$dirty"
+        class="icon is-small is-right"
+      >
+        <i class="fas fa-times"></i>
+      </span>
+    </BaseTextTypeInput>
+    <BaseTextTypeInput
+      type="number"
+      :label="$t('form.age')"
+      v-model="userData.age"
+      :errors="v$.userData.age.$errors"
+    />
+    <BaseTextTypeInput
+      :label="$t('form.email')"
+      type="email"
+      v-model="userData.email"
+      :errors="v$.userData.email.$errors"
+      icons-placement="has-icons-left"
+    >
+      <span class="icon is-small is-left">
+        <i class="fas fa-envelope"></i>
+      </span>
+    </BaseTextTypeInput>
+    <BaseSelect
+      :options="[
+        $t('form.gender.options.male'),
+        $t('form.gender.options.female'),
+        $t('form.gender.options.non-binary'),
+      ]"
+      :label="$t('form.gender.description')"
+      v-model="userData.gender"
+      :placeholder="$t('form.gender.options.placeholder')"
+      :errors="v$.userData.gender.$errors"
+    />
+    <BaseTextArea
+      :label="$t('form.message.label')"
+      :placeholder="$t('form.message.placeholder')"
+      v-model="userData.comments"
+      :errors="v$.userData.comments.$errors"
+    />
+    <BaseMultipleCheckbox
+      :question="$t('form.offer.description')"
+      :options="['Google', 'LinkedIn', 'Facebook', 'Instagram', 'Twitter']"
+      v-model="userData.marketing"
+    />
+    <BaseCheckbox
+      :label="$t('form.newsletter')"
+      v-model="userData.subscribe"
+      :errors="v$.userData.subscribe.$errors"
+    />
+    <BaseRadio
+      name="offer"
+      :options="[$t('form.terms.options.yes'), $t('form.terms.options.no')]"
+      :question="$t('form.terms.description')"
+      v-model="userData.terms"
+      :errors="v$.userData.terms.$errors"
+    />
+
+    <div class="field is-grouped">
+      <div class="control">
+        <button type="submit" class="button is-link" :disabled="isFormInvalid">
+          {{ $t('form.buttons.submit') }}
+        </button>
+      </div>
+      <div class="control">
+        <button class="button is-link is-light">
+          {{ $t('form.buttons.cancel') }}
+        </button>
+      </div>
     </div>
-    <div class="control">
-      <button class="button is-link is-light">
-        {{ $t('form.buttons.cancel') }}
-      </button>
-    </div>
-  </div>
+  </form>
 </template>
 
 <script lang="ts">
